19bb96c659cf3dd7fce3857110266accc01385d0
manoj-kumar-git <manoj.k@doodleblue.in>
2025-05-23 18:19:45 +0530

    table almost done

--- Node/src/routes/auth.js
+++ Node/src/routes/auth.js
@@ -21,6 +21,7 @@ router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   console.log(email,password,"email")
   const user = await User.findOne({ email });
+  console.log(user,"user")
   if (!user) return res.status(400).json({ message: 'Invalid credentials', });
 
   const isMatch = await bcrypt.compare(password, user.password);
